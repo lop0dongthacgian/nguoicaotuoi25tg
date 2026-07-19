be92fdaeb7121a2c3971ef9cef97a1c3494e9654
lop0dongthacgian <lop0dongthacgian@gmail.com>
2026-07-20 05:38:35 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// ⚠️ Điền URL Web App Apps Script của bạn vào đây sau khi deploy (Deploy > New deployment > Web app)
+// URL có dạng: https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_rcV1AAU-WKA7FanMVqPXnhWWTIgGp-jOLqFF0Tk9yZVk--Wm6-y8dm4FUVgt5v4/exec";
