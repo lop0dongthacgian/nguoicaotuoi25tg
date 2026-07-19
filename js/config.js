73d4915fda3da945ae74ddfeff0669d6fc29417d
lop0dongthacgian <lop0dongthacgian@gmail.com>
2026-07-20 05:53:51 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
 // ⚠️ Điền URL Web App Apps Script của bạn vào đây sau khi deploy (Deploy > New deployment > Web app)
 // URL có dạng: https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_rcV1AAU-WKA7FanMVqPXnhWWTIgGp-jOLqFF0Tk9yZVk--Wm6-y8dm4FUVgt5v4/exec";
+
+// Bật chế độ debug để xem log chi tiết
+const DEBUG = true;
